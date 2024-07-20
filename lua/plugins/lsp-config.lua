9e427e93c172146f89f4253e59aec030430421c5
breekristensen <bree.kristensen@gmail.com>
2024-07-20 14:06:19 -0500

    added various lsps

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -196,7 +196,17 @@ return { -- LSP Configuration & Plugins
     -- for you, so that they are available from within Neovim.
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
-      'stylua', -- Used to format Lua code
+      'stylua', -- Lua
+      'cssls', -- CSS
+      'jsonls', -- JSON
+      'tsserver', -- Javascript
+      'marksman', -- Markdown
+      'somesass_ls', -- SCSS
+      'taplo', -- Toml
+      'vimls', -- VIM
+      'yamlls', -- YAML
+      'pyright', -- Python
+      'bashls', -- Bash
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
